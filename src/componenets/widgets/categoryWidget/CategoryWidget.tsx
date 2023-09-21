4ad2c27d1864a0216a78aedef40040f2d3ef21d5
Maxwell Knight <maxwell.knight98@gmail.com>
2023-09-21 03:21:54 +0300

    translation fixes and CategoryWidget refactorization

--- src/componenets/widgets/categoryWidget/CategoryWidget.tsx
+++ src/componenets/widgets/categoryWidget/CategoryWidget.tsx
@@ -1,35 +1,45 @@
 import { Dropdown } from '../..';
-import { useTranslation } from 'react-i18next';
 import { useFetch } from '../../../hooks';
 import { FetchResponse } from '../../../types';
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
 import './categoryWidget.css';
-import { useEffect } from 'react';
+
+type Option = {
+	label: string,
+	value: string
+}
+type OptionsType = Option[];
 
 type Categories = {
 	categories: string[]
 }
 const CategoryWidget = () => {
 	const { t } = useTranslation();
+	const [options, setOptions] = useState<OptionsType>();
 	const { data , loading }: FetchResponse<Categories | null | undefined> = 
 		useFetch('http://localhost:3001/info/budget/categories');
-
-	useEffect(() => {
-		if(!loading && data) data.categories.unshift('empty');
-	}, [loading])
-
+	
 	const parseCategories = (categoris: string[]) => categoris.map(category => ({
 		label: t(`translation:categories.${category}`),
 		value: category
 	}))
+	
+	useEffect(() => {
+		if(!loading && data){
+			data.categories.unshift("empty");
+			setOptions(() => parseCategories(data.categories))
+		}
+	}, [loading]) 
 
 	return (
 		<div className="category-widget">
-		{!loading && data && 
+		{!loading && data && options &&
 			<div className='category-widget'>
-				<Dropdown label={t("translation:category")} options={parseCategories(data.categories)} />
-				<Dropdown options={parseCategories(data.categories)} />
-				<Dropdown options={parseCategories(data.categories)} />
-				<Dropdown options={parseCategories(data.categories)} />
+				<Dropdown label={t("translation:category")} options={options} />
+				<Dropdown options={options} />
+				<Dropdown options={options} />
+				<Dropdown options={options} />
 			</div>
 		}
 		</div>
