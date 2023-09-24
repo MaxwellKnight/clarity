e3b4a2706b10958a0b31b13518d63b8597b08e24
Maxwell Knight <maxwell.knight98@gmail.com>
2023-09-24 11:35:43 +0300

    categorywidget types fixed

--- src/componenets/widgets/categoryWidget/CategoryWidget.tsx
+++ src/componenets/widgets/categoryWidget/CategoryWidget.tsx
@@ -6,15 +6,12 @@ import { useEffect, useState } from 'react';
 import './categoryWidget.css';
 
 type Option = { label: string, value: string } [];
-
-type Categories = {
-	categories: string[]
-}
+type Categories = { categories: string[] }
 type CategoriesFetch = Categories | null | undefined;
 
 const CategoryWidget = () => {
 	const { t } = useTranslation();
-	const [options, setOptions] = useState<Option>();
+	const [options, setOptions] = useState<Option>([]);
 	const { data , loading }: FetchResponse<CategoriesFetch> = useFetch('http://localhost:3001/info/budget/categories');
 	
 	const parseCategories = (categoris: string[]) => categoris.map(category => ({
@@ -31,13 +28,13 @@ const CategoryWidget = () => {
 
 	return (
 		<div className="category-widget">
-		{!loading && data && options &&
+		{!loading ?
 			<div className='category-widget'>
 				<Dropdown label={t("translation:category")} options={options} />
 				<Dropdown options={options} />
 				<Dropdown options={options} />
 				<Dropdown options={options} />
-			</div>
+			</div> : null
 		}
 		</div>
 	)
